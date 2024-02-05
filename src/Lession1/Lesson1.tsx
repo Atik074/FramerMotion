77b672fae8d35d91c4a53ec72fcc9269597a2295
Atik074 <atiksmats@gmail.com>
2024-02-05 23:20:06 +0600

    whiletap,whilefocus,whilehover animation

--- src/Lession1/Lesson1.tsx
+++ src/Lession1/Lesson1.tsx
@@ -1,12 +1,112 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
+
+const parent = {
+  hidden: { opacity: 0, scale: 0.6 },
+  visible: { opacity: 1, scale: 1 },
+  tap: {
+    scale: 1.1,
+    rotate: 360,
+    transition: {
+      duration: 3,
+    },
+  },
+};
+const child = {
+  hidden: { opacity: 0, scale: 0.5 },
+  visible: { opacity: 1, scale: 1 },
+};
+
 const Lesson1 = () => {
+  const parentRef = useRef(null);
+
   return (
-    <div>
-      <motion.div className="w-[200px] h-[200px] bg-green-900  rounded-lg"
-          initial={{rotate:0}}
-          animate={{rotate: 30}}
-          transition={{duration:3}}
-      ></motion.div>
+    <div ref={parentRef}>
+      <motion.div
+        className="p-4 shadow-2xl w-[300px] bg-white rounded-lg "
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileTap="tap"
+        whileDrag={{
+          scale: 1.3,
+        }}
+        drag
+        dragConstraints={parentRef}
+        transition={{
+          ease: "easeInOut",
+          duration: 1.5,
+          staggerChildren: 1,
+        }}
+      >
+        <div className="flex  gap-x-[8px] mb-2">
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+        </div>
+        <div className="flex gap-x-[8px] mb-2">
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+        </div>
+        <div className="flex gap-x-[8px] mb-2">
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+          <motion.div
+            className="size-20 bg-green-500 rounded shadow-lg"
+            variants={child}
+          ></motion.div>
+        </div>
+
+        {/* <motion.div
+          className="size-20 bg-green-500 rounded shadow-lg"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-green-500 rounded  shadow-lg"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-green-500 rounded shadow-lg"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-green-500 rounded shadow-lg"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-green-500 rounded shadow-lg"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-green-500 rounded shadow-lg"
+          variants={child}
+        ></motion.div> */}
+      </motion.div>
     </div>
   );
 };
